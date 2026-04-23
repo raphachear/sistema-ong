e5485e164053e000a900bee24b341cb17473ce1c
Raphaela Chear <raphachear@outlook.com>
2026-04-22 22:15:26 -0300

    ativando cadastro com banco

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require("mysql2");
 const path = require("path");
 
 const app = express();
@@ -6,12 +7,39 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/api/teste", (req, res) => {
-  res.send("Servidor funcionando!");
+const db = mysql.createConnection({
+  host: "mysql.railway.internal",
+  user: "root",
+  password: "FaJhwkZcSpsIDBZIhKjJIvbrFvaZngmZ",
+  database: "railway"
+});
+
+db.connect(err => {
+  if (err) {
+    console.error("Erro ao conectar:", err);
+  } else {
+    console.log("Conectado ao MySQL!");
+  }
+});
+
+// CADASTRO REAL
+app.post("/voluntarios", (req, res) => {
+  const { nome, cpf, email } = req.body;
+
+  const sql = "INSERT INTO voluntarios (nome, cpf, email) VALUES (?, ?, ?)";
+
+  db.query(sql, [nome, cpf, email], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Erro ao cadastrar");
+    }
+
+    res.send("Voluntário cadastrado com sucesso!");
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log("Servidor rodando na porta " + PORT);
 });
