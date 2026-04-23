35581baf17bb645c181ab97da9d54aa725dd4278
Raphaela Chear <raphachear@outlook.com>
2026-04-22 22:45:29 -0300

    versão final funcionando

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+// CONEXÃO MYSQL (RAILWAY)
 const db = mysql.createConnection({
   host: "roundhouse.proxy.rlwy.net",
   user: "root",
@@ -14,15 +15,26 @@ const db = mysql.createConnection({
   database: "railway",
   port: 20614
 });
-db.connect(err => {
+
+// NÃO DEIXA O SERVIDOR QUEBRAR
+db.connect((err) => {
   if (err) {
-    console.error("Erro ao conectar:", err);
+    console.error("Erro ao conectar no MySQL:", err);
   } else {
     console.log("Conectado ao MySQL!");
   }
 });
 
-// CADASTRO REAL
+db.on("error", (err) => {
+  console.error("Erro no MySQL:", err);
+});
+
+// ROTA DE TESTE
+app.get("/teste", (req, res) => {
+  res.send("API funcionando!");
+});
+
+// CADASTRO
 app.post("/voluntarios", (req, res) => {
   const { nome, cpf, email } = req.body;
 
@@ -34,7 +46,7 @@ app.post("/voluntarios", (req, res) => {
       return res.status(500).send("Erro ao cadastrar");
     }
 
-res.send("TESTE 123");
+    res.send("Voluntário cadastrado com sucesso!");
   });
 });
 
